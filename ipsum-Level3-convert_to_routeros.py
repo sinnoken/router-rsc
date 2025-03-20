2fd6c320321143038c9a9cd6fc5fbdc4cfc60dfe
hsieh c. yi-cheng <sinnoken@live.com>
2025-03-20 11:41:20 +0000

    update

--- ipsum-Level3-convert_to_routeros.py
+++ ipsum-Level3-convert_to_routeros.py
@@ -2,6 +2,11 @@ import requests
 
 # 下載 IP 清單
 url = 'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/3.txt'
+#
+comment = 'IPsum-Threat-Intelligence-Feed'
+#
+list    = 'HN-BLACKLIST-IPSUM-L3'
+
 response = requests.get(url)
 ip_list = response.text.splitlines()
 
@@ -13,4 +18,4 @@ with open(output_file, 'w') as f:
         if line.startswith('#') or not line.strip():
             continue
         ip_address = line.split()[0]  # 假設 IP 位址在每行的第一個位置
-        f.write(f'/ip firewall address-list add list=drop_traffic address={ip_address}/32\n')
+        f.write(f'/ip firewall address-list add address={ip_address} comment={comment} list={list}\n')
