cbbbfee85718326f44102827edfa2ec4651c2931
hsieh c. yicheng <sinnoken@live.com>
2025-03-22 16:35:05 +0800

    Update ipsum-Level3-convert_to_routeros.py

--- ipsum-Level3-convert_to_routeros.py
+++ ipsum-Level3-convert_to_routeros.py
@@ -1,21 +1,40 @@
+import os
 import requests
 
-# 下載 IP 清單
-url = 'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/3.txt'
-#
+# 確保目錄存在
+output_dir = './rsc/'
+os.makedirs(output_dir, exist_ok=True)
+
+# 設定下載的 URL 清單
+urls = [
+    'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/1.txt',
+    'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/2.txt',
+    'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/3.txt',
+    'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/4.txt',
+    'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/5.txt',
+    'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/6.txt',
+    'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/7.txt',
+    'https://github.com/stamparm/ipsum/raw/refs/heads/master/levels/8.txt'
+]
+
+# RouterOS 設定
 comment = 'IPsum-Threat-Intelligence-Feed'
-#
-list    = 'HN-BLACKLIST-IPSUM-L3'
 
-response = requests.get(url)
-ip_list = response.text.splitlines()
+for url in urls:
+    level = url.split('/')[-1].split('.')[0].replace('level', '')  # 取得 Level 數字
+    list_name = f'HN-BLACKLIST-IPSUM-L{level}'
+    output_file = os.path.join(output_dir, f'ipsum-Level{level}.rsc')
 
-# 轉換成 RouterOS 指令
-output_file = 'rsc/ipsum-Level3.rsc'
-with open(output_file, 'w') as f:
-    for line in ip_list:
-        # 跳過註解行
-        if line.startswith('#') or not line.strip():
-            continue
-        ip_address = line.split()[0]  # 假設 IP 位址在每行的第一個位置
-        f.write(f'/ip firewall address-list add address={ip_address.ljust(15)} comment={comment} list={list}\n')
+    # 下載資料
+    response = requests.get(url)
+    ip_list = response.text.splitlines()
+
+    # 轉換並寫入 RouterOS 指令
+    with open(output_file, 'w') as f:
+        for line in ip_list:
+            if line.startswith('#') or not line.strip():  # 跳過註解或空行
+                continue
+            ip_address = line.split()[0]  # 取每行的第一個欄位作為 IP
+            f.write(f'/ip firewall address-list add address={ip_address.ljust(15)} comment={comment} list={list_name}\n')
+
+    print(f'已儲存 {output_file}')
